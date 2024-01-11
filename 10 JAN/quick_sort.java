f961e463046eb86622c02fed6fbe4d365abd8ecd
Jatin <jatinsingh31185@gmail.com>
2024-01-11 11:16:20 +0530

    10 JAN

---
+++ 10 JAN/quick_sort.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class quick_sort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int cases=sc.nextInt();
+        for(int i=0;i<cases;i++){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int count=0;
+            for(int j=0;j<x;j++){
+            if(sc.nextInt()==count+1)
+            count++;}
+            System.out.println(Math.ceilDiv(x-count, y));
+        }
+    }
+}
